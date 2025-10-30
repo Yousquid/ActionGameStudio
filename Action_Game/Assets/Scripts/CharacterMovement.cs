12fa8fee8f1ea09d8a81546f841cfd41004c2792
Yousquid <ly2456@nyu.edu>
2025-10-31 03:17:40 +0800

    basic_movement_1/2

--- Action_Game/Assets/Scripts/CharacterMovement.cs
+++ Action_Game/Assets/Scripts/CharacterMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class CharacterMovement : MonoBehaviour
 {
     public float acceleration = 10f;    // 加速度
-    public float maxSpeed = 6f;         // 最大速度
+    public float maxSpeed = 12f;         // 最大速度
     public float deceleration = 8f;     // 松开按键时的减速度
     public float rotationSpeed = 10f;   // 转向速度（用于让角色朝向移动方向）
 
@@ -13,6 +13,7 @@ public class CharacterMovement : MonoBehaviour
     private Rigidbody rb;
     private Vector3 moveVelocity;
     private Vector3 camForward, camRight, camLeft, camBackward;
+    private KeyCode counterDirectionKey;
 
     public enum MoveState
     {
@@ -38,19 +39,12 @@ public class CharacterMovement : MonoBehaviour
 
     void ReadCameraBasis()
     {
-        if (camera)
-        {
             camForward = camera.forward;
             camRight = camera.right;
             camLeft = -camera.right;
             camBackward = -camera.forward; ;
-        }
-        else
-        {
-            camForward = Vector3.forward;
-            camRight = Vector3.right;
-            
-        }
+        
+       
 
         camForward.y = 0f; camRight.y = 0f;
         camForward.Normalize();
@@ -61,13 +55,33 @@ public class CharacterMovement : MonoBehaviour
     private void InputDetection()
     {
 
-        RotateCharacterWhileIdle();
+        if (Input.GetKey(KeyCode.W))
+        {
+            counterDirectionKey = KeyCode.S;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            counterDirectionKey = KeyCode.W;
 
-        //RotateCharacterWhile
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            counterDirectionKey = KeyCode.D;
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            counterDirectionKey = KeyCode.A;
+
+        }
+
+        RotateCharacterWhileIdle();
+        StopCharacterWhileHoldingOppositeDirections();
+        RotateCharacterWhileWalkAndRun();
     }
     void StateDetection()
     {
-        if (rb.linearVelocity.magnitude > 0f)
+        if (rb.linearVelocity.magnitude > 0f && rb.linearVelocity.magnitude <= 4f)
         {
             characterMoveState = MoveState.Walk;
         }
@@ -76,30 +90,66 @@ public class CharacterMovement : MonoBehaviour
             characterMoveState = MoveState.Idle;
 
         }
+        else if (rb.linearVelocity.magnitude >= 4f)
+        {
+            characterMoveState = MoveState.Run;
+
+        }
+    }
+
+    void StopCharacterWhileHoldingOppositeDirections()
+    {
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
+    void RotateCharacterWhileWalkAndRun()
+    {
+        if ((Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.D)) ||
+           (Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.A)))
+        {
+            rb.linearVelocity = Vector3.zero;
+
+            
+        }
+
+        if ((Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.S)) ||
+            (Input.GetKey(KeyCode.S) && Input.GetKeyDown(KeyCode.W)))
+        {
+            rb.linearVelocity = Vector3.zero;
+
+            
+        }
     }
     private void RotateCharacterWhileIdle()
     {
         if (characterMoveState == MoveState.Idle)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W)|| Input.GetKey(KeyCode.W))
             {
                 Vector3 flatCamForward = camForward.normalized;
                 Quaternion targetRot = Quaternion.LookRotation(flatCamForward, Vector3.up);
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 1000f * Time.deltaTime);
             }
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A))
             {
                 Vector3 flatCamForward = camLeft.normalized;
                 Quaternion targetRot = Quaternion.LookRotation(flatCamForward, Vector3.up);
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 1000f * Time.deltaTime);
             }
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.S))
             {
                 Vector3 flatCamForward = camBackward.normalized;
                 Quaternion targetRot = Quaternion.LookRotation(flatCamForward, Vector3.up);
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 1000f * Time.deltaTime);
             }
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D))
             {
                 Vector3 flatCamForward = camRight.normalized;
                 Quaternion targetRot = Quaternion.LookRotation(flatCamForward, Vector3.up);
@@ -110,6 +160,18 @@ public class CharacterMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        StopCharacterWhileHoldingOppositeDirections();
+        bool oppositeHeld = (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) ||
+                       (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S));
+
+        if (oppositeHeld)
+        {
+            rb.linearVelocity = Vector3.zero;           // 清零
+                                                        // 可选：一点阻尼，防抖更稳
+                                                        // rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, Vector3.zero, deceleration * Time.fixedDeltaTime);
+            return;                                     // 关键：不要再加力了
+        }
+
         HandleMovement();
     }
 
